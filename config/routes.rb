f783ea5147478d5a50406a945891ee544924efbb
Mohammad Mehdi Aslamy <aslamimehdi@gmail.com>
2019-01-19 10:41:38 -0800

    layout design partially completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/services'
-  get 'static_pages/inventory'
-  get 'static_pages/contact'
-  get 'static_pages/faqs'
+  #get '/home',       to: 'static_pages/home'
+  get '/services',   to: 'static_pages#services'
+  get '/inventory',  to: 'static_pages#inventory'
+  get '/contact',    to: 'static_pages#contact'
+  get '/faqs',       to: 'static_pages#faqs'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
