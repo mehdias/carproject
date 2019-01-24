244be68d388afd47f04eba38094d2d317cda6816
mehdias <41026478+mehdias@users.noreply.github.com>
2019-01-24 14:10:20 -0800

    rails setup (#5)
    * add static pages controller
    
    * added sample static pages
    
    * layout design partially completed
    
    * installed devise and set links
    
    * implemenation for rails setup

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def services
+  end
+
+  def inventory
+  end
+
+  def contact
+  end
+
+  def faqs
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
+  root 'static_pages#home'
+  #get '/home',       to: 'static_pages/home'
+  get '/services',   to: 'static_pages#services'
+  get '/inventory',  to: 'static_pages#inventory'
+  get '/contact',    to: 'static_pages#contact'
+  get '/faqs',       to: 'static_pages#faqs'
+  resources :requests
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
